24dd4fde81e1360d98df42c16da790d1c1fee47d
itoromoreno <itoromoreno@cobiscorp.com>
2020-02-21 14:45:09 -0500

    view controller for classes

--- cities/src/class/class.module.ts
+++ cities/src/class/class.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ClassApiController } from "./class.api.controller";
 import { ClassService } from "./class.service";
 import { ClassEntity } from "./class.entity";
+import { ClassViewController } from "./class.view.controller";
 
 @Module({
     controllers:[
         ClassApiController,
+        ClassViewController,
     ],
     providers: [
         ClassService,
---
+++ cities/src/class/class.view.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Res } from "@nestjs/common";
+
+@Controller('class')
+export class ClassViewController {
+
+
+    @Get()
+    getClasses(
+        @Res() res,
+    ) {
+        res.render('class/routes/show-class');
+    }
+}
